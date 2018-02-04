e99b87a269e8485348e477a1b36433829b342821
Jeroen Domburg <git@j0h.nl>
2018-02-04 14:55:17 +0800

    Add SPI LCD as menuconfig option, clean up scripts to flash ROM/HD

--- components/tme-esp32/component.mk
+++ components/tme-esp32/component.mk
@@ -1,2 +1,11 @@
 #Component makefile
 
+COMPONENT_SRCDIRS := .
+COMPONENT_OBJS := hd.o hexdump.o localtalk.o main.o mpu6050.o mpumouse.o snd.o
+
+ifdef CONFIG_TME_DISP_MIPI
+COMPONENT_OBJS += mipi_lcd.o adns9500.o
+endif
+ifdef CONFIG_TME_DISP_WROVER
+COMPONENT_OBJS += spi_lcd.o
+endif
--- flashhd.sh
+++ flashhd.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-#python /home/jeroen/esp8266/esp32/esp-idf/bin/esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud 115200 write_flash -z -fs 32m 0x100000 doom1-cut.wad
-python /home/jeroen/esp8266/esp32/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x120000 $1
+
+if [ -n "$1" ]; then
+	echo "Usage: $0 /dev/ttyUSBx hard_disk_file.bin"
+fi
+
+PORT=/dev/ttyUSB1
+FILE=hard_disk.bin
+if [ -n "$1" ]; then PORT=$1; fi
+if [ -n "$2" ]; then FILE=$2; fi
+
+python $IDF_PATH/components/esptool_py/esptool/esptool.py --chip esp32 --port $PORT --baud $((921600/2)) --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size detect 0x120000 $FILE
 
